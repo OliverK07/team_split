e13299c07f83d35a0a4d0c52f7fe5648b08ba2be
oliverke <thusnoy@gmail.com>
2017-12-08 10:04:11 +0800

    remove seed generator.

--- team_split.py
+++ team_split.py
@@ -1,6 +1,5 @@
 import random
 from operator import itemgetter
-# import randint
 
 a =  int(input("Enter a team number: "))
 lst = [[] for _ in xrange(a)]
@@ -14,32 +13,16 @@ while True:
    if data[0] == '':break
    seed.append((data[0],int(data[1])))
 rfile.close()
-        
-
-# print seed
-
-for i in range(ord('A'), ord('Z')+1):
-   seed.append((chr(i),random.randrange(5)+1))
-# print seed
 
 for i in range(0,a):
-   # print i
-   # print sum_list[i]
    sum_list[i] = sum( ( pair[0] for pair in lst[i] ) )
-# print 'final sum_list:',sum_list
-
 
 for value in sorted(seed, key=itemgetter(1), reverse=True):
-   # print value
    for i in range(0,a):
-      # print i
-      # print sum_list[i]
       sum_list[i] = sum( [ pair[1] for pair in lst[i] ] )
-   # print 'sum_list:',sum_list
-   # sum_list.append(lst[i] for i in a)
-   # print 'sum_lst:',sum_list
+
    index = sum_list.index(min(sum_list))
-   # print index
+
    flag = False
    for i in range(index+1,a):
       if sum_list[index] == sum_list[i]:
@@ -56,9 +39,7 @@ for i in range(0,a):
    print 'Team',i+1,':',lst[i]
    wfile.writelines('Team'+`i+1`+':'+`lst[i]`)
    wfile.write('\n')
-   # print '\n'
+
    print 'Member:',len(lst[i]),'Total Point:', sum_list[i]
    wfile.writelines('Member:'+`len(lst[i])`+'     Total Point:'+ `sum_list[i]`)
    wfile.write('\n******************************\n')
-   # print '\n'
-   # print 'Total Point:', sum_list[i]
